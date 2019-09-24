6aefa3f78a13edba8508ec8a9d08fe9cfe207c4f
Bidyut Kumar Patra <SESA503898@schneider-electric.com>
2019-09-24 20:29:55 +0530

    sample service

---
+++ sample/sample/Controllers/TrainingController.cs
@@ -0,0 +1,42 @@
+﻿using sample.Models;
+using sample.Services;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace sample.Controllers
+{
+    [RoutePrefix("trainings")]
+    public class TrainingController : ApiController
+    {
+        public ITrainingService TrainingService { get; set; }
+
+        public TrainingController(ITrainingService trainingService)
+        {
+            this.TrainingService = trainingService;
+        }
+
+        [HttpGet]
+        [Route("")]
+        public async Task<IHttpActionResult> GetAllTrainingData()
+        {
+            var result = await this.TrainingService.GetAllTrainingData();
+            return Ok(result);
+        }
+        
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IHttpActionResult> GetTrainingData(int id)
+        {
+            var result = await Task.Run(() => new TrainingData());
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [Route("")]
+        public async Task<IHttpActionResult> SaveTrainingData([FromBody] TrainingData trainingData)
+        {
+            var result = await this.TrainingService.SaveTrainingData(trainingData);
+            return Ok(result);
+        }
+    }
+}
---
+++ sample/sample/Models/TrainingData.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace sample.Models
+{
+    public class TrainingData : Entity<Guid>
+    {
+        [JsonProperty("trainingName")]
+        public string Name { get; set; }
+        [JsonProperty("startDate")]
+        public DateTime StartDate { get; set; }
+        [JsonProperty("endDate")]
+        public DateTime EndDate { get; set; }
+    }
+}
