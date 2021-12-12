1fe83b6f1f864ec5ca1a33d99ce21094a49e3a21
Bidyut Kumar Patra <SESA503898@schneider-electric.com>
2021-12-13 00:17:59 +0530

    secure storage

--- riddles/Program.cs
+++ riddles/Program.cs
@@ -10,6 +10,9 @@ namespace Riddles
     {
         static void Main(string[] args)
         {
+            var guid = Guid.NewGuid();
+            var guidStr = guid.ToString().ToUpper();
+
             var riddle1 = new SortIntsOnDiskFile()
             {
                 Input = @"C:\WORK@SE\Test\unsorted_numbers.txt",
